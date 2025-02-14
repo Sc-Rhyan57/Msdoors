1adc6d5c63ae7e82047ff0d4f63f466519de57ad
Sc-Rhyan57 <rhyanlus501@gmail.com>
2025-02-14 18:30:02 -0300

    SERVICES!

---
+++ Src/Services.lua
@@ -0,0 +1,16 @@
+return {
+    Lighting = game:GetService("Lighting"),
+    Players = game:GetService("Players"),
+    ReplicatedStorage = game:GetService("ReplicatedStorage"),
+    RunService = game:GetService("RunService"),
+    SoundService = game:GetService("SoundService"),
+    TextChatService = game:GetService("TextChatService"),
+    UserInputService = game:GetService("UserInputService"),
+    PathfindingService = game:GetService("PathfindingService"),
+    ProximityPromptService = game:GetService("ProximityPromptService"),
+    TweenService = game:GetService("TweenService"),
+    Workspace = game:GetService("Workspace"),
+    HttpService = game:GetService("HttpService"),
+    LocalPlayer = game:GetService("Players").LocalPlayer
+}
+print("[Msdoors] • Serviços carregados!")
